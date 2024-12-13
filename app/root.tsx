e779958b433eecec808c5a9e2718ee1ae948e8b1
menfem <cjr@menfem.com>
2024-12-13 06:35:20 +0000

    make a change to the logo

--- app/root.tsx
+++ app/root.tsx
@@ -253,10 +253,10 @@ function Logo() {
 	return (
 		<Link to="/" className="group grid leading-snug">
 			<span className="font-light transition group-hover:-translate-x-1">
-				epic
+				Men
 			</span>
 			<span className="font-bold transition group-hover:translate-x-1">
-				not!
+				Fem
 			</span>
 		</Link>
 	)
