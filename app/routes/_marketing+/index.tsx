7269eeffcfe004cd980fa845634cc2fa3b725aa6
menfem <cjr@menfem.com>
2024-12-17 20:35:10 +0000

    added h1 of menfem to homepage

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -5,7 +5,9 @@ export const meta: MetaFunction = () => [{ title: 'Home' }]
 export default function Index() {
 	return (
 		<main className="min-h-screen">
-			<div className="container mx-auto p-4">{/* Add your content here */}</div>
+			<div className="container mx-auto p-4">
+				<h1>Menfem</h1>
+			</div>
 		</main>
 	)
 }
